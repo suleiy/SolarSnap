d3e6c483352cdd847a7e5f235c00b9c0eb3301a6
Zoe Myatt <73500422+zoemyatt@users.noreply.github.com>
2023-06-27 15:11:43 -0400

    added ai image api

--- solarsnap/rooftop_panels/views.py
+++ solarsnap/rooftop_panels/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import *
 import time
+import replicate
 
 
 def index(request):
@@ -19,5 +20,10 @@ def index(request):
 def success(request):
     latest_image = Image.objects.order_by('id').last()
     latest_image_url = latest_image.uploaded_image.url
-    return render(request, 'confirmation.html', {'latest_image_url': latest_image_url})
+    #return render(request, 'confirmation.html', {'latest_image_url': latest_image_url})
+    output = replicate.run(
+        "arielreplicate/instruct-pix2pix:10e63b0e6361eb23a0374f4d9ee145824d9d09f7a31dcd70803193ebc7121430",
+        input={'input_image': open('../solarsnap/solarsnap'+ latest_image_url, 'rb'), 'instruction_text':"add solar panels to roof"}
+    )
+    return render(request, 'confirmation.html', {'latest_image_url': output})
 
